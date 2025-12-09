d14c7ffb3b922f920e4c8ea5181f8d8b2105bce3
ttosi <ttosi519@gmail.com>
2025-12-09 09:42:06 -0600

    add blank-screen script

---
+++ blank-screen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+while true; do
+    if pgrep mpv >/dev/null; then
+        sudo sh -c "echo 0 > /sys/class/graphics/fb0/blank"
+    else
+        sudo sh -c "echo 1 > /sys/class/graphics/fb0/blank"
+    fi
+    sleep 1
+Done
